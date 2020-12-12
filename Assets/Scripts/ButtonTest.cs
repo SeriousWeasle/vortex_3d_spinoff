0776e7aa22e3554adb179635e43e45713eeb18c7
SeriousWeasle <seriousweasle@gmail.com>
2020-12-12 13:37:28 +0100

    added main menu

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    public Image background;
+    public Text selectedMode;
+    public GameObject[] circles;
+
+    private void Start()
+    {
+        UpdateMenu(0);
+    }
+
+    public void UpdateMenu(int idx)
+    {
+        switch(idx)
+        {
+            case 0: //easy
+                background.color = new Color(0.6f, 0f, 0.6f);
+                selectedMode.color = new Color(0.4f, 0f, 0.4f);
+                selectedMode.text = "Easy";
+                break;
+            case 1: //normal
+                background.color = new Color(0f, 0.6f, 0f);
+                selectedMode.color = new Color(0f, 0.4f, 0f);
+                selectedMode.text = "Normal";
+                break;
+            case 2: //hard
+                background.color = new Color(0.6f, 0f, 0f);
+                selectedMode.color =new Color(0.4f, 0f, 0f);
+                selectedMode.text = "Hard";
+                break;
+            case 3: //very hard
+                background.color = new Color(0.2f, 0.2f, 0.2f);
+                selectedMode.color = new Color(0f, 0f, 0f);
+                selectedMode.text = "Very Hard";
+                break;
+            default: //default to easy
+                background.color = new Color(0.6f, 0f, 0.6f);
+                selectedMode.color = new Color(0.4f, 0f, 0.4f);
+                selectedMode.text = "Easy";
+                break;
+        }
+
+        for (int i = 0; i < circles.Length; i++)
+        {
+            if (i == idx)
+            {
+                circles[i].SetActive(true);
+                print(i);
+            }
+            else
+            {
+                circles[i].SetActive(false);
+            }
+        }
+    }
+    public void selectEasy()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void selectNormal()
+    {
+        Debug.Log("selected normal");
+    }
+
+    public void selectHard()
+    {
+        Debug.Log("selected hard");
+    }
+
+    public void selectVeryHard()
+    {
+        Debug.Log("selected very hard");
+    }
+}
---
+++ Assets/Scripts/OpenOriginal.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class OpenOriginal : MonoBehaviour
+{
+    public void open()
+    {
+        Application.OpenURL("https://www.khanacademy.org/computer-programming/vortex-13-combat-update/6304869942624256");
+    }
+}
