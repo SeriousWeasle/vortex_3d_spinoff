2d63e966d2a1bf05261448bccc24c9748d3bba62
SeriousWeasle <seriousweasle@gmail.com>
2020-12-12 13:50:17 +0100

    added basic enemy scripting

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int diff;
+    public Transform target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        gameObject.transform.LookAt(target); //face player
+        gameObject.transform.position += gameObject.transform.forward * (diff + 2) * Time.deltaTime;
+    }
+}
